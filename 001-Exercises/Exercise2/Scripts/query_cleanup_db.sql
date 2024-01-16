0a4aab2873b96793321ab6eb5bfa720d5f959ac2
avadombi <vp.adombi@gmail.com>
2024-01-16 00:37:52 -0500

    Add a query file to remove all tables and schema as well as its dependencies such as views, tables, and all other objects

---
+++ 001-Exercises/Exercise2/Scripts/query_cleanup_db.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS country_db.countries;
+DROP TABLE IF EXISTS country_db.cities;
+DROP TABLE IF EXISTS country_db.currencies;
+DROP TABLE IF EXISTS country_db.languages;
+
+-- Delete all tables, views and all other objects
+-- our schema contains
+DROP DATABASE IF EXISTS country_db;
