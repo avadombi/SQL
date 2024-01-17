5f9a53067a1554c4c2f35841fc76893397c69936
avadombi <vp.adombi@gmail.com>
2024-01-17 11:32:54 -0500

    add a file to remove no matching data in both countries and cities based on country_code_2 field

--- 001-Exercises/Exercise2/Scripts/CreateDBCleanData/remove_no_match_data.sql
+++ 001-Exercises/Exercise2/Scripts/CreateDBCleanData/remove_no_match_data.sql
@@ -1,3 +1,7 @@
 -- No match in countries table
-DELETE FROM
-cleaned_db.countries
+DELETE FROM cleaned_db.countries
+WHERE country_code_2 IN ('aq', 'bv', 'cc', 'io', 'tf');
+
+-- No match in cities table
+DELETE FROM cleaned_db.cities
+WHERE country_code_2 IN ('bq', 'md', 'sh', 'tz');
